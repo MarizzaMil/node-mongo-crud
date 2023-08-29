3557a52b4282dea2b15099e4017cf1f7e2ad72f5
MarizzaMil <marizza.mil89@gmail.com>
2023-08-29 11:22:08 +0300

    conn

--- db.js
+++ db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(MONGODB_URI).then(() => {
   console.log('MongoDB connected');
 }).catch((err) => {
   console.error('MongoDB connection error:', err);
